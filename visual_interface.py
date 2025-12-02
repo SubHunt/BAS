e84e6f9ffbee395a392c49a27ce5b06b45b737a2
SubHunt <phaeton.kz@gmail.com>
2025-12-03 01:00:58 +0300

    checkbutton

--- visual_interface.py
+++ visual_interface.py
@@ -3,7 +3,7 @@ import tkinter as tk
 
 def button_clicked():
     text = entry.get()
-    label.config(text=f"Hello world{text}")
+    label.config(text=f"Hello world {text}")
 
 
 root = tk.Tk()
@@ -11,10 +11,13 @@ root.title("Первая программа с интерфейсом.")
 root.geometry("300x200")
 
 label = tk.Label(root, )
-label.pack(pady=20)
+label.pack(pady=10)
 
 entry = tk.Entry(root)
-entry.pack(pady=30)
+entry.pack(pady=20)
+
+lever = tk.Checkbutton(root, text="Светодиоды")
+lever.pack(pady=3)
 
 button = tk.Button(root, text="Нажми", command=button_clicked)
 button.pack(pady=10)
